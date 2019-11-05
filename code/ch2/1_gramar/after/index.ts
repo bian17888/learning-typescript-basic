c08826313a6c364026af3c19a8ec02b399616737
zhuzhen.bk <zhuzhen.bk@alibaba-inc.com>
2019-11-05 15:12:06 +0800

    demo2

---
+++ code/ch2/1_gramar/after/index.ts
@@ -0,0 +1,11 @@
+var num1 = 100;
+var num2 = 20;
+var myName = "beck";
+
+function addNumber(n1: number, n2: number, n3: number) {
+  var result = n1 + n2 + n3;
+  var msg = "Sum is : " + result;
+  return msg;
+}
+
+alert(addNumber(num1, num2, 12));
