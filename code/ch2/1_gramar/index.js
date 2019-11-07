30d4462f1e1f4ed38caae73172cf5d5c03a46508
zhuzhen.bk <zhuzhen.bk@alibaba-inc.com>
2019-11-07 11:29:47 +0800

    update : folder structure

---
+++ code/ch2/1_gramar/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var ch2_1_gramar;
+(function (ch2_1_gramar) {
+    var num1 = 100;
+    var num2 = 20;
+    var myName = "beck";
+    function addNumber(n1, n2, n3) {
+        var result = n1 + n2 + n3;
+        var msg = "Sum is : " + result;
+        return msg;
+    }
+    alert(addNumber(num1, num2, 520));
+})(ch2_1_gramar || (ch2_1_gramar = {}));
---
+++ code/ch2/1_gramar/index_before.js
@@ -0,0 +1,13 @@
+"use strict";
+var ch2_1_gramar_before;
+(function (ch2_1_gramar_before) {
+    var num1 = 100;
+    var num2 = 20;
+    var myName = "beck";
+    function addNumber(n1, n2, n3) {
+        var result = n1 + n2 + n3;
+        var msg = "Sum is : " + result;
+        return msg;
+    }
+    alert(addNumber(num1, num2, myName));
+})(ch2_1_gramar_before || (ch2_1_gramar_before = {}));
